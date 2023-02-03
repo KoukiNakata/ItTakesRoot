3b19f227b64a5265c8e898f831038c4031ed6081
KoukiNakata <n.kouki.12.25@gmail.com>
2023-02-04 01:42:20 +0900

    Kouki/issue-9-StartEnd02
    add Button

---
+++ Assets/Scripts/GameEnd.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnd : MonoBehaviour
+{
+    public bool flag_gameEnd;//true.ゲームエンド　false.非ゲームエンド
+    [SerializeField] GameObject RetryButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flag_gameEnd = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    //ゲームエンド時の処理　ゲームエンドでこの処理を呼び出す
+    void gameEnd()
+    {
+        flag_gameEnd = true;
+        Debug.Log("ゲームエンド");
+        //ゲームエンド時の処理
+        //スコア表示
+        //リトライボタンの表示
+        RetryButton.SetActive(true);
+
+    }
+
+    
+
+}
